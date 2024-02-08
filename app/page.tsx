49b7e470fc6445fc3dbe547333a3a90591d920e0
Aman Lama <lama.aman1998@gmail.com>
2024-02-08 12:04:26 +0545

    final tmp

--- app/page.tsx
+++ app/page.tsx
@@ -119,6 +119,21 @@ export default function Home() {
           <code>Please get your pages ready to dive into coding. You are free to customize your button as you want.</code>
           </p>
         </Link>
+        <Link
+          href="/akshyat"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Akshyat{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <code>Please get your pages ready to dive into coding. You are free to customize your button as you want.</code>
+          </p>
+        </Link>
       </div>
     </main>
   );
